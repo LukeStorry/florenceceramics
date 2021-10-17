57c2fde19a27db7b24a36f407edc6823418bef9e
Luke Storry <luke@lukestorry.co.uk>
2021-10-17 14:53:38 +0000

    added image resizing shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const htmlmin = require('html-minifier');
-const lazyImages = require("eleventy-plugin-lazyimages");
 const fs = require("fs");
 const path = require("path");
+const Image = require("@11ty/eleventy-img");
 
 const htmlMinTransform = (value, outputPath) => {
     if (outputPath.indexOf('.html') < 0)
@@ -20,19 +20,30 @@ const getCss = () => {
     const cssFilePath = path.resolve(__dirname, "./src/_includes/styles.css");
     return fs.readFileSync(cssFilePath);
 }
-const renderImageGrid = () => {
-    return "";
-};
+const imageShortcode = (src, alt) => {
+    const options = {
+        widths: [300, 600, null],
+        formats: ["jpeg"],
+        outputDir: "./dist/img/"
+    };
+    Image(src, options);
+    const metadata = Image.statsSync(src, options);
+    let imageAttributes = {
+        alt,
+        sizes: "(min-width: 400px) 300px, (min-width: 700px) 600px, 1000px",
+        loading: "lazy",
+        decoding: "async",
+    };
+    return Image.generateHTML(metadata, imageAttributes);
+}
 
 module.exports = function (config) {
     config.addPassthroughCopy({ "assets/passthrough": "." });
-    config.addPassthroughCopy({ "assets/img": "img" });
-    config.addPlugin(lazyImages, { cacheFile: "" });
 
     config.addTransform("htmlmin", htmlMinTransform);
 
+    config.addShortcode("image", imageShortcode);
     config.addShortcode("getCss", getCss);
-    config.addShortcode("renderImageGrid", renderImageGrid);
     global.helpers = config.javascriptFunctions;
     config.setPugOptions({ globals: ['helpers'] });
 
