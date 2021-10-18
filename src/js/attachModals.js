c41b5b617401d6dabd3efe5ae5a4f3b3827ba3c0
Luke Storry <luke@lukestorry.co.uk>
2021-10-18 11:47:20 +0000

    added modal clicks

--- src/js/attachModals.js
+++ src/js/attachModals.js
@@ -1,11 +1,31 @@
-function openModal(src) {
-    console.log(src);
+var modal = document.getElementById("modal")
+
+function openModal(event, img) {
+    if (event instanceof KeyboardEvent && event.key !== 'Enter' && event.key !== ' ') return;
+    modal.firstChild.focus()
+    modal.lastChild.setAttribute("src", img.src);
+    modal.lastChild.setAttribute("srcset", img.srcset);
+    modal.lastChild.setAttribute("alt", img.alt);
+    modal.classList.add("active");
+}
+
+function closeModal(event) {
+    if (event && event.target.tagName == "IMG") return;
+    modal.classList.remove("active");
 }
 
 var pictures = document.getElementsByClassName("picture")
 Array.prototype.forEach.call(pictures, function(element) {
     element.setAttribute("role", "button");
     element.setAttribute("tabindex", "0");
-    var src = element.firstChild.getAttribute("src");
-    element.setAttribute("onclick", "openModal('" + src + "')");
+    element.setAttribute("onClick", "openModal(event, this.firstChild)");
+    element.setAttribute("onKeyDown", "openModal(event, this.firstChild)");
+    element.classList.add("pointer");
+
+})
+
+window.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        closeModal()
+    }
 })
